6d171223f447251911acc37c545609c78f19386f
xhihi0x <wong23a@mtholyoke.edu>
2014-10-23 13:18:56 -0400

    First commit

---
+++ src/Drinker.java
@@ -0,0 +1,15 @@
+
+public class Drinker implements Runnable {
+	
+	int r;
+	public Drinker(int _r){
+		r = _r;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	
+	int[] a = new int[3]; //TODO put in constructor?q
+	private Thread thirsty1;
+	private Thread thirsty2;
+	private Thread thirsty3;
+
+	private Thread Server;
+	
+	
+}
